a6829c6871c2e18bfff6a5b1df8c9262bcff1a1e
EC2 Default User <ec2-user@ip-172-31-34-205.ap-northeast-1.compute.internal>
2022-11-02 07:42:39 +0000

    [Finish]2-5

--- score.rb
+++ score.rb
@@ -8,3 +8,13 @@ if score >= 50 || (score <= 100 && score >= 80)
   puts "得点は50点以上、または80点以上100点以下です"
 end
 
+score = 80
+
+if (score >= 60 || score <= 120) && score >= 100
+  puts "得点は60点以上または120点以下で、かつ100点以上です"
+end
+
+if score >= 60 || (score <= 120 && score >= 100)
+  puts "得点は60点以上、または100点以上120点以下です"
+end
+
