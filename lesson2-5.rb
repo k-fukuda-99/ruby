cefcfa434f68f3d3b47470878016599ddb64506b
EC2 Default User <ec2-user@ip-172-31-34-205.ap-northeast-1.compute.internal>
2022-11-02 07:44:29 +0000

    [Finish]2-5

---
+++ lesson2-5.rb
@@ -0,0 +1,13 @@
+total_price = 100
+if total_price <= 120
+  puts "みかんを購入。所持金に余りあり"
+end
+
+if total_praice = 100
+  puts "みかんを購入。所持金は0円"
+end
+
+if total_praice >= 70
+  puts "みかんを購入することができません"
+end
+
