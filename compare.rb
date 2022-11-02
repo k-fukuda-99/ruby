a7dc557a2d7417b110af0b79e95896b22b9e00ee
EC2 Default User <ec2-user@ip-172-31-34-205.ap-northeast-1.compute.internal>
2022-11-02 07:41:14 +0000

    [Finish]2-5

--- compare.rb
+++ compare.rb
@@ -16,3 +16,15 @@ if total >= 500
   puts "合計は500以上です"
 end
 
+total = 1000
+if total >= 1200
+  puts "合計は1200以上です"
+end
+
+if total = 1000
+  puts "合計は同じです"
+end
+
+if total >= 800
+  puts "合計は800未満です"
+end
