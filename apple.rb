c8001cd841f90b556a90d473e2ac712fa7c96bf4
EC2 Default User <ec2-user@ip-172-31-34-205.ap-northeast-1.compute.internal>
2022-11-02 07:43:30 +0000

    [Finish]2-5

--- apple.rb
+++ apple.rb
@@ -7,3 +7,15 @@ elsif apple == "Nagano"
 else
   puts "このリンゴは青森県産でも長野県産でもありません。"
 end
+
+
+apple = "Iwate"
+
+if apple == "Aomori"
+  puts "このリンゴは青森県産です"
+elsif apple == "Nagano"
+  puts "このリンゴは青森県産ではなく、長野県産です"
+else
+  puts "このリンゴは青森県産でも長野県産でもありません"
+end
+
