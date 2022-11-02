1af8f4dbf2a6c32ec102f0400a7ccb8ba274d76a
EC2 Default User <ec2-user@ip-172-31-34-205.ap-northeast-1.compute.internal>
2022-11-02 01:51:13 +0000

    [Finish]2-4

--- weight.rb
+++ weight.rb
@@ -4,3 +4,10 @@ weight = 50
 puts name +"さんの体重は" + weight.to_s + "kgです"
 puts "#{name}さんの体重は#{weight * 2}kgです"
 puts '#{name}さんの体重は#{weight}kgです'
+
+name = "B"
+weight = 50
+
+puts name +"さんの体重は" + weight.to_s + "kgです"
+puts "#{name}さんの体重は#{weight * 2}kgです"
+puts '#{name}さんの体重は#{wight}kgです'
