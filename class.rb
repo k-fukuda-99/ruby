eab089430903dc3782591f35700b251dcdb77e1b
EC2 Default User <ec2-user@ip-172-31-34-205.ap-northeast-1.compute.internal>
2022-11-04 14:12:44 +0000

    [Finish]2-9

--- class.rb
+++ class.rb
@@ -1,8 +1,17 @@
+array = [4,6,8]
+puts array.class
+puts array.join("*")
+
+string = "Thank you"
+puts string.class
+puts string.join("*")
+
 array = [1,2,3]
 puts array.class
 puts array.join("*")
 
-string = "Hwllo"
+string = "Hello"
 puts string.class
 puts string.join("*")
+
 
