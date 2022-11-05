5fb0e0e8ade9aef735301a6066709f74b9f7d9e2
EC2 Default User <ec2-user@ip-172-31-34-205.ap-northeast-1.compute.internal>
2022-11-05 00:44:43 +0000

    [Finish]2-10

--- inheritance.rb
+++ inheritance.rb
@@ -12,3 +12,16 @@ bus.run(5)
 
 puts Bus.superclass
 
+class Bullet_train
+  def run(distance)
+    puts "電車で#{distance}キロ走ります"
+  end
+end
+
+class Bullet_train < Erectrik_train
+end
+
+erectrik_train = Erectrik_train.new
+erectrik_train.run(1200)
+
+puts erectrik_train.superclass
